4a6270c2933d89b180213cba0e7f6adb3d83d2c6
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-10-05 08:40:58 -0700

    add uglify, debug

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -9,13 +10,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new UglifyJsPlugin(),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Interplanetary Death Counter',
       template: './src/index.html',
       inject: 'body'
     })
-  ]
+  ],
   module: {
     rules: [
       {
