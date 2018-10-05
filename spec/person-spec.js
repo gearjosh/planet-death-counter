76b77a3725b6516fb933bee275453ebb27bb82d9
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-10-05 16:12:00 -0700

    add test for young people cheating death

--- spec/person-spec.js
+++ spec/person-spec.js
@@ -102,4 +102,10 @@ describe('Person', function() {
     expect(actual).toEqual(control);
   });
 
+  it('should tell you that you have yet to cheat death if you have not lived past average life expectancy', function() {
+    let actual = elevenYearOldGirl.jupiterDeathCheater();
+    let control = "you have yet to cheat death";
+    expect(actual).toEqual(control);
+  });
+
 });
