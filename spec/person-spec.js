ddd58bc250705786d1e3dc6019e3cf05022adc23
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-10-05 13:53:20 -0700

    add first spec, intend it to fail

--- spec/person-spec.js
+++ spec/person-spec.js
@@ -1,5 +1,17 @@
 import { Person } from './../src/person.js';
 
 describe('Person', function() {
-  
-}
+  let elevenYearOldGirl;
+  let ninetyYearOldMan;
+
+  beforeEach(function() {
+    elevenYearOldGirl = new Person(11, female);
+    ninetyYearOldMan = new Person(90, male);
+  });
+
+  it('should tell you your age in earth years', function() {
+    let actual = Person.earthConverter(elevenYearOldGirl);
+    let control = 11;
+    expect(actual).toEqual(control);
+  });
+};
